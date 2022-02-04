9cd83ae6313e8fae3b74bcadcddca5f296fad3db
Babatunde Atijosan <atijosanbabatunde@gmail.com>
2022-02-04 07:41:08 +0100

    floe

---
+++ src/web/pages/index.js
@@ -0,0 +1,8 @@
+export { default as Landing } from './Landing';
+export { default as E_404 } from './E404';
+export { default as Single } from './Single';
+export { default as SignIn } from './SignIn';
+export { default as SignUp } from './SignUp';
+export { default as Checkout } from './Checkout';
+export { default as Privacy } from './Privacy';
+export { default as Terms } from './Terms';
